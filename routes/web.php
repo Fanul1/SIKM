d4410b41468693908c1feda974c46b6d4e031b01
Fanul1 <fanul@mhs.unsyiah.ac.id>
2023-10-31 11:07:57 +0700

    feat: add dashboard admin

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ use App\Http\Controllers\SikmController;
 Route::get('sikm', [SikmController::class, 'home']);
 Route::get('/', [SikmController::class, 'home']);
 
+Route::get('/dashboard', function () {
+    return view('user.admin.dashboard');
+});
+
 Route::get('sikm/login', [SikmController::class, 'login'])->name('login')->middleware('guest');
 Route::post('/login', [SikmController::class, 'authenticate']);
 Route::post('/logout', [SikmController::class, 'logout']);
